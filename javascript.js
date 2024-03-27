543eebd2a95c648933b27a7328a5d9f5cca1277d
emmailmoniemi <emma.ilmoniemi@gmail.com>
2024-03-27 15:10:40 +0100

    Finish game console version.

--- javascript.js
+++ javascript.js
@@ -9,34 +9,65 @@ function getComputerChoice() {
     }
 }
 
-function playOneRound() {
 
+
+function playRound() {
 
     let computerSelection = getComputerChoice();
     let playerSelection = (prompt("Please enter your choice")).toLowerCase();
-
     console.log(playerSelection);
     console.log(computerSelection);
 
     if (playerSelection == computerSelection) {
         return `Draw, ${playerSelection} equals ${computerSelection}`;
     }
-    switch (playerSelection, computerSelection) {
-        case 'rock', 'paper':
+    switch (playerSelection + computerSelection) {
+        case 'rockpaper':
             return 'You Lose! Paper beats Rock';
-        case 'paper', 'scissors':
+        case 'paperscissors':
             return 'You Lose! Scissors beats Paper';
-        case 'scissors', 'rock':
+        case 'scissorsrock':
             return 'You Lose! Rock beats Scissors';
-        case 'rock', 'scissors':
+        case 'rockscissors':
             return 'You Win! Rock beats Scissors';
-        case 'scissors', 'paper':
+        case 'scissorspaper':
             return 'You Win! Scissors beats Paper';
-        case 'paper', 'rock':
+        case 'paperrock':
             return 'You Win! Paper beats Rock';
         default:
             return 'Something went wrong. Was your input correct?';
     }
 }
 
-console.log(playOneRound());
+function playGame() {
+
+    let userScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        console.log("Round " + (i + 1));
+        console.log(result);
+
+        if(result.includes("You Win")) {
+            userScore++;
+            console.log("Your Score: " + userScore + " Computer Score: " + computerScore);
+        } else if(result.includes("You Lose")) {
+            computerScore++;
+            console.log("Your Score: " + userScore + " Computer Score: " + computerScore);
+        } else {
+            console.log("Your Score: " + userScore + " Computer Score: " + computerScore);
+        }
+
+    }
+
+    if(userScore > computerScore) {
+        console.log("Congrats you won the GAME")
+    } else if(computerScore > userScore) {
+        console.log("Lol you lose, sry git gud")
+    } else {
+        console.log("Draw. Play again?");
+    }
+}
+
+playGame();
