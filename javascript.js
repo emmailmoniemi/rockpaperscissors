f717dfe717dc6fea703d8d49c324d93abd89bee9
emmailmoniemi <emma.ilmoniemi@gmail.com>
2024-07-18 14:42:47 +0200

    Add UI.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,17 @@
+const buttons = document.querySelectorAll("button");
+const buttonscont = document.querySelector(".buttonscont");
+const playerscoretext = document.querySelector("#playerscore");
+const computerscoretext = document.querySelector("#computerscore");
+const roundtext = document.querySelector(".roundtext");
+const resulttext = document.querySelector("#resultcontainer");
+const body = document.querySelector("body");
+
+
+const endgamecont = document.createElement("div");
+endgamecont.classList.add("endgamecont");
+endgamecont.setAttribute("style", "font-size: 24px; background: yellow; font-weight: bold");
+
+
 function getComputerChoice() {
     let rand = Math.random();
     if (rand < 0.33) {
@@ -11,31 +25,55 @@ function getComputerChoice() {
 
 
 
-function playRound() {
+function playRound(playerSelection) {
 
     let computerSelection = getComputerChoice();
-    let playerSelection = (prompt("Please enter your choice")).toLowerCase();
+    //let playerSelection = (prompt("Please enter your choice")).toLowerCase();
     console.log(playerSelection);
     console.log(computerSelection);
 
     if (playerSelection == computerSelection) {
-        return `Draw, ${playerSelection} equals ${computerSelection}`;
+        resulttext.textContent = `Draw, ${playerSelection} equals ${computerSelection}`;
+        return 'draw';
     }
     switch (playerSelection + computerSelection) {
         case 'rockpaper':
-            return 'You Lose! Paper beats Rock';
+            console.log('You Lose! Paper beats Rock');
+            resulttext.textContent = `You Lose! Paper beats Rock`;
+            return 'loss';
         case 'paperscissors':
-            return 'You Lose! Scissors beats Paper';
+            resulttext.textContent = `You Lose! Scissors beats Paper`;
+            return 'loss';
         case 'scissorsrock':
-            return 'You Lose! Rock beats Scissors';
+            resulttext.textContent = `You Lose! Rock beats Scissors`;
+            return 'loss';
         case 'rockscissors':
-            return 'You Win! Rock beats Scissors';
+            resulttext.textContent = `You Win! Rock beats Scissors`;
+            return 'win';
         case 'scissorspaper':
-            return 'You Win! Scissors beats Paper';
+            resulttext.textContent = `You Win! Scissors beats Paper`;
+            return 'win';
         case 'paperrock':
-            return 'You Win! Paper beats Rock';
+            resulttext.textContent = `You Win! Paper beats Rock`;
+            return 'win';
         default:
-            return 'Something went wrong. Was your input correct?';
+            console.log('Something went wrong. Was your input correct?');
+            return 'draw';
+    }
+}
+
+function endGame(userScore, computerScore) {
+
+    body.insertBefore(endgamecont, roundtext);
+    body.removeChild(buttonscont);
+
+    if(userScore > computerScore) {
+        console.log("Congrats you won the GAME");
+        endgamecont.textContent = "Congrats you won the GAME";
+    } else if(computerScore > userScore) {
+        endgamecont.textContent = "Lol you lose, sry git gud";
+    } else {
+        endgamecont.textContent = "Draw. Play again?";
     }
 }
 
@@ -43,31 +81,32 @@ function playGame() {
 
     let userScore = 0;
     let computerScore = 0;
+    let round = 1;
 
-    for (let i = 0; i < 5; i++) {
-        let result = playRound();
-        console.log("Round " + (i + 1));
-        console.log(result);
-
-        if(result.includes("You Win")) {
-            userScore++;
+    buttons.forEach((button) => {
+        // and for each one we add a 'click' listener
+        button.addEventListener("click", function (e) {
+          if(computerScore < 5 && userScore < 5) {
+            console.log("Round " + round);
+            let result = playRound(e.target.id);
+            if(result == 'win') {
+                userScore++;
+                playerscoretext.textContent = `PLAYER: ${userScore}`;
+            } else if(result == "loss") {
+                computerScore++;
+                computerscoretext.textContent = `COMPUTER: ${computerScore}`;
+            } 
             console.log("Your Score: " + userScore + " Computer Score: " + computerScore);
-        } else if(result.includes("You Lose")) {
-            computerScore++;
-            console.log("Your Score: " + userScore + " Computer Score: " + computerScore);
-        } else {
-            console.log("Your Score: " + userScore + " Computer Score: " + computerScore);
+            round++;
+            roundtext.firstElementChild.textContent = `ROUND: ${round}`;
+        } if(computerScore > 4 || userScore > 4) {
+            endGame(userScore, computerScore);
         }
+        });
+      });
 
-    }
 
-    if(userScore > computerScore) {
-        console.log("Congrats you won the GAME")
-    } else if(computerScore > userScore) {
-        console.log("Lol you lose, sry git gud")
-    } else {
-        console.log("Draw. Play again?");
-    }
+   
 }
 
 playGame();
